9c2d2e8a73dbca6cf1244446dd22353c5d58802c
Jack Thompson <jackthompsoninfo@gmail.com>
2019-09-29 00:37:01 -0400

    jobs and db working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(("Test"), max_length=50)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
