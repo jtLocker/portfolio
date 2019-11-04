e326bb936145074f30d0b79719b46201e03da623
Jack <jlt20@indiana.edu>
2019-11-04 11:36:46 -0500

    adding my info

--- port/settings.py
+++ port/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_^kk113q%(-z@vis@tucbp_ml&8a^ie2qo*5dhxxg=i_52g-r7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'www.jackt.info']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'www.jackt.info']
 
 
 # Application definition
